5a07b99820c4e238774ac0407edad682766e03b9
rsgc-tao-l <ltao@rsgc.on.ca>
2017-05-12 19:55:44 -0400

    Add files via upload
    updated the annotation of coding and improved the spacing. final copy?

--- Calculator-UIKit-Assignment/Calculator/Calculator.swift
+++ Calculator-UIKit-Assignment/Calculator/Calculator.swift
@@ -25,71 +25,64 @@ class Calculator {
         operation = Operation.addition
         updateState()
     }
+    
     //set the calculator operation to minus.
     func subtraction() {
         operation = Operation.subtraction
         updateState()
     }
-    //set the curent operaton to percentage.
+    
+    //divide the current operated number by 100 to display its decimal form as a number out of 100.
     func percentage() {
+        // if the provided value is empty , the following operation can calculate the answer using the computedValue.
         if providedValue == "" {
+            // by using a seperate variable, the main variables like calculated and provided value wont be affected and thus would have less chance of having a crashing error.
             if var y = computedValue{
                 y = y / 100
-                
+                //make the provided value a string version of the variable y.
                 providedValue = String(format: "%g", y)
             }
+            //if the providedvalue is present instead, the code will get the answer using the provided value.
         }else {
+            // by using a seperate variable, the main variables like calculated and provided value wont be affected and thus would have less chance of having a crashing error.
             if var y = Double(providedValue) {
                 y = y / 100
-                
+                //make the provided value a string version of the variable y.
                 providedValue = String(format: "%g", y)
             }
         }
     }
     
-    
     //Sets calculator operation to multiplication, and computes a new value, if needed.
-    
     func multiply() {
         
         // Set the operation
         operation = Operation.multiplication
         updateState()
     }
+    
+    // changes the number given or operated on from negative to positive or positive to negave depending on its original value.
     func plusminus() {
+        //again, if the providedvalue is not give, the operation will be done to the computedValue and displayed using the providedValue. it should be noted that whether computedvalue is nill or not is not important in this case, for it did not force unwrap it and therefore will not crash.
         if providedValue == "" {
+            //the operation will be done to the temporary variable to reduce overall impact on the general code.
             if var x = computedValue{
                 x = x * -1
-                
+                //make the provided value a string version of the variable y.
                 providedValue = String(format: "%g", x)
             }
+            //same logic, if the providedvalue is given instead, the code will simply do the calculation on it and display it.
         }else {
+            //the operation will be done to the temporary variable to reduce overall impact on the general code.
             if var x = Double(providedValue) {
                 x = x * -1
-                
+                //make the provided value a string version of the variable y.
                 providedValue = String(format: "%g", x)
             }
         }
     }
-    //        if providedValue != ""{
-    //            if computedValue != nil{
-    //                if providedValue != "" {
-    //                    computedValue = Double(providedValue)! * -1
-    //                } else {
-    //                    computedValue = computedValue! * -1
-    //                }
-    //            }else {
-    //                makeProvidedValueComputedValue()
-    //                computedValue = computedValue! * -1
-    //            }
-    //        } else {
-    //            computedValue = computedValue! * -1
-    //        }
-    //        x = x * -1
-    //    }
-    /**
-     Sets calculator operation to division, and computes a new value, if needed.
-     */
+    
+    //set the operation to divide.
     func divide() {
         
         // Set the operation
@@ -179,5 +172,4 @@ class Calculator {
         computedValue = nil
         updateState()
     }
-    
 }
--- Calculator-UIKit-Assignment/Calculator/ViewController.swift
+++ Calculator-UIKit-Assignment/Calculator/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     //connecton between the interface and code
     @IBOutlet weak var Label: UILabel!
+    //drawing up the connection between the currentOperation label with the code
     @IBOutlet weak var currentOperation: UILabel!
     //to bring out the calculator class
     var model = Calculator()
@@ -19,6 +20,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         //clean up the label
         Label.text = ""
+        //the code to display the operation when given
         currentOperation.text = ""
     }
     
@@ -26,94 +28,136 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    //hooked up button
+    //hooking up the buttons
+    //linking the 0 button on the interface to the code.
     @IBAction func zeroPressed(_ sender: Any) {
         model.addToNewValue(digit: "0")
         Label.text = model.providedValue
     }
+    
+    //linking the 1 button on the interface to the code.
     @IBAction func onePressed(_ sender: Any) {
         model.addToNewValue(digit: "1")
         Label.text = model.providedValue
     }
+    
+    //linking the 2 button on the interface to the code.
     @IBAction func twoPressed(_ sender: Any) {
         model.addToNewValue(digit: "2")
         Label.text = model.providedValue
     }
+    
+    //linking the 3 button on the interface to the code.
     @IBAction func threePressed(_ sender: Any) {
         model.addToNewValue(digit: "3")
         Label.text = model.providedValue
     }
+    
+    //linking the 4 button on the interface to the code.
     @IBAction func fourPressed(_ sender: Any) {
         model.addToNewValue(digit: "4")
         Label.text = model.providedValue
     }
+    
+    //linking the 5 button on the interface to the code.
     @IBAction func fivePressed(_ sender: Any) {
         model.addToNewValue(digit: "5")
         Label.text = model.providedValue
     }
+    
+    //linking the 6 button on the interface to the code.
     @IBAction func sixPressed(_ sender: Any) {
         model.addToNewValue(digit: "6")
         Label.text = model.providedValue
     }
+    
+    //linking the 7 button on the interface to the code.
     @IBAction func sevenPressed(_ sender: Any) {
         model.addToNewValue(digit: "7")
         Label.text = model.providedValue
     }
+    
+    //linking the 8 button on the interface to the code.
     @IBAction func eightPressed(_ sender: Any) {
         model.addToNewValue(digit: "8")
         Label.text = model.providedValue
     }
+    
+    //linking the 9 button on the interface to the code.
     @IBAction func ninePressed(_ sender: Any) {
         model.addToNewValue(digit: "9")
         Label.text = model.providedValue
     }
+    
+    //linking the percentage button on the interface to the code.
     @IBAction func percentage(_ sender: Any) {
         model.percentage()
+        // the following if statement prevents the unexpected crashing of this code if pressed without an providedValue.
         if model.providedValue != ""{
             Label.text = model.providedValue
         }
         
     }
+    
+    // when the plusminus button is pressed, the following code will run triggering the functions in the calculator.swift
     @IBAction func plusminus(_ sender: Any) {
         model.plusminus()
+        //this will prevent the code from crashing when pressing the plusminus button without an providedvalue.
         if model.providedValue != ""{
             Label.text = model.providedValue
         }
         
     }
+    
+    //linking the clear button on the interface to the code.
     @IBAction func clear(_ sender: Any) {
         model.clear()
+        // reset the display for calculation and operation
         Label.text = ""
         currentOperation.text = ""
     }
     
+    //linking the addition button on the interface to the code.
     @IBAction func addition(_ sender: Any) {
         model.addition()
         currentOperation.text = "+"
     }
+    
+    //linking the equal button on the interface to the code.
     @IBAction func subtraction(_ sender: Any) {
         model.subtraction()
         currentOperation.text = "-"
     }
+    
+    //linking the decimaldot button on the interface to the code.
     @IBAction func decimaldot(_ sender: Any) {
+        // the if statement prevents the code from crashing when pressed without an providedvalue.
         if model.providedValue != "."{
             model.addToNewValue(digit: ".")
             Label.text = model.providedValue
         }
     }
+    
+    //linking the divide button on the interface to the code.
     @IBAction func divide(_ sender: Any) {
         model.divide()
         currentOperation.text = "÷"
     }
+    
+    //linking the multiply button on the interface to the code.
     @IBAction func multiply(_ sender: Any) {
         model.multiply()
         currentOperation.text = "x"
     }
+    
+    // linking the equal button on the interface to the code.
     @IBAction func equal(_ sender: Any) {
         model.equals()
+        //prevent crash
         if model.computedValue != nil{
             Label.text = String(format: "%g", model.computedValue!)
         }
+        //reset the operaton display.
         currentOperation.text = ""
     }
     
